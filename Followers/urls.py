2634dfa33b6598fa13e07b8454d477ca051adfbd
EmmaRubiH <emmahjorting89@gmail.com>
2022-12-14 10:19:37 +0000

    added filters to post and profiles

--- Followers/urls.py
+++ Followers/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from Followers import views
 
 urlpatterns = [
-    path('followers/', views.FollowerList.as_view()),
+    path('Followers/', views.FollowerList.as_view()),
     path('followers/<int:pk>/', views.FollowerDetail.as_view())
 ]

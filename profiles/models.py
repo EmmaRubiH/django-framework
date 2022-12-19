8e83b9754d1f94def2cbc7ef271d452c2ca955f8
EmmaRubiH <emmahjorting89@gmail.com>
2022-12-19 08:49:16 +0000

    add image

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_qdjgyp'
+        upload_to='images/', default='../https://res.cloudinary.com/emmarubih/image/upload/v1670764476/default_profile_tqv16g'
     )
 
     class Meta:
